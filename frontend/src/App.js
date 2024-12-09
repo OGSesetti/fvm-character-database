ba736df39b91f1f65f2368cb3295b0041976eb8f
OGSesetti <severi.tahtinen2@gmail.com>
2024-12-09 20:19:27 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React, { Children, useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header.jsx'
 import HomeButton from './Homebutton.jsx'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { createContext, useContext } from 'react';
-import { ApiProvider } from './ApiContext';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {createContext, useContext} from 'react';
+import {ApiProvider} from './ApiContext';
 
 import Home from "./pages/Home.jsx";
 import Games from "./pages/Games.jsx";
